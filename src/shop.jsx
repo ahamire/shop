5f4b8df7ca367a08c7c579401a2384761392a6d4
ahamire <gfueo3243@gmail.com>
2023-12-18 16:03:15 +0200

    begin

---
+++ src/shop.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function Shop() {
+  const [count, setCount] = useState(0)
+  const slides = ['/img/1.png','/img/2.png'];
+  return (
+       <>
+       <main></main>
+       <footer></footer>
+       </>
+  )
+}
+
+export default Shop;
